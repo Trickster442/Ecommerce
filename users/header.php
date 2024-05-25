77a0b2a4eb842421623cdf0bc41a71b0bd037f35
Trickster <reone054@gmail.com>
2024-05-25 22:33:34 +0545

    nav

---
+++ users/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <script src="https://kit.fontawesome.com/487ae9d97a.js" crossorigin="anonymous"></script>
+    <title>User</title>
+</head>
+<body>
+<header>
+            <nav class="top">
+                <div class="logo">
+                   <a href="/users/frontend/index.html">
+                    Logo
+                   </a> 
+                </div>
+                <form class="search-form">
+                    <input type="text" placeholder="Search...">
+                    <input type="submit" name="submit" value="submit">    
+                </form>
+                <div class="pointer">
+                    <ul class="button">
+                        <li>
+                            <a><i class="fa-solid fa-right-to-bracket"></i></a>
+                        </li>
+                        <a><li><i class="fa-solid fa-circle-xmark"></i></li></a>
+                        <a><li><i class="fa-solid fa-cart-shopping"></i></li></a>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
