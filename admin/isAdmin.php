c33a26f4fcd7b7b4e3ac82fdb23380a124a6b316
Trickster <reone054@gmail.com>
2024-06-21 12:32:24 +0545

    product

--- admin/isAdmin.php
+++ admin/isAdmin.php
@@ -5,36 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Home Page</title>
     <link rel="stylesheet" href="isAdmin.css">
-    <!-- font awesome cdn -->
+    <?php
+    include('header.php');
+
+?>
 </head>
 <?php
-session_start();
 
 if(!$_SESSION['users']){
 header("location:form/login.php");
 }
+
+
 ?>
 <body>
-    <nav>
-        <div class="Logo">
-            Logo
-        </div>
-        <ul class="navigation-link">
-            <li><a href="#">Hello, <?php
-
-            echo $_SESSION['users'];
-            ?>
-            </a></li>
-            <li><a href="form/logout.php">Logout</a></li>
-            <li><a href="#">User Panel</a></li>
-        </ul>
-    </nav>
 
     <div>
         <h2 class="dashboard">Dashboard</h2>
     </div>
     <div class="secondary-nav">
-        <a href="product/products.php">Add Post</a>
+        <a href="./product/products.php">Add Post</a>
         <a href="category/categories.php">Add Category</a>
     </div>
 </body>
--- admin/product/products.php
+++ admin/product/products.php
@@ -1,3 +1,5 @@
+<!-- index.php -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,9 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="products.css">
     <title>Product Page</title>
+    <?php
+    include('header.php');
+    ?>
 </head>
+
 <?php
-session_start();
 
 if (!$_SESSION["users"]) {
     header("location:../form/login.php");
@@ -16,99 +21,96 @@ if (!$_SESSION["users"]) {
 ?>
 
 <body>
-    <form action="insert.php" method="POST" enctype="multipart/form-data">
-        <div class="top">
-            <p>Product Details:</p>
-        </div>
-        <div class="product-name">
-            <label for="pname">Product Name:</label>
-            <input type="text" id="pname" name="pname" placeholder="Enter Product Name" required>
-        </div>
-        </div>
-
-        <div class="product-price">
-            <label for="pprice">Product Price:</label>
-            <input type="text" id="pprice" name="pprice" placeholder="Enter Product Price" required>
-        </div>
-        </div>
-
-        <div class="product-image">
-            <label for="pimage">Add Product Image:</label>
-            <input type="file" id="pimage" name="pimage" required>
-        </div>
-        </div>
-
-        <div class="product-stock">
-            <label for="pstock">Product Stock:</label>
-            <input type="text" id="pstock" name="pstock" placeholder="Enter Stock Amount">
-        </div>
-        </div>
-
-                    
-        
-        <div class="product-category">
-    <label for="pcat">Select Product Category:</label>
-    <select id="pcat" name="pcat" required>
-        <option selected>Select Category</option>
-        <?php
-        include "config.php";
-        $sql = "SELECT * FROM `category` WHERE status = 'active'";
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_assoc()) {
-            echo "<option value='{$row['category_name']}'>{$row['category_name']}</option>";
-        }
-        ?>
-    </select>
-</div>
-        <button name="submit" type="submit" class="upload">Upload</button>
-    </form>
-    <!-- fetch data -->
-    <div class="table-section">
-        <table>
-            <thead>
-                <th>S.N.</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Image</th>
-                <th>Stock</th>
-                <th>Category</th>
-                <th>Delete</th>
-                <th>Update</th>
-            </thead>
-            <tbody>
-                <?php
-                include 'config.php';
-
-                $record = mysqli_query($conn, "SELECT p.*, c.category_name 
-                               FROM product p
-                               INNER JOIN category c ON p.category_id = c.id");
-                $count = 1;
-                while ($row = mysqli_fetch_array($record)) {
-                    echo "
-        <tr>
-            <td>{$count}</td>
-            <td>$row[product_name]</td>
-            <td>$row[price]</td>
-            <td><img src='$row[product_image]' height='50px' width='50px'></td>
-            <td>$row[stock]</td>
-            <td>$row[category_name]</td>
-            <td>
-            <div class='delete'>
-                <a href='delete.php?id=" . $row['id'] . "'>Delete</a>
-                </div>
-            </td>
-            <td>
-            <div class='update'>
-                <a href='update.php?id=" . $row['id'] . "'>Update</a>
+    <div class="clearfix">
+        <form action="insert.php" method="POST" enctype="multipart/form-data">
+            <div class="top">
+                <p>Product Details:</p>
             </div>
-            </td>
-        </tr>
-    ";
-    $count += 1;
-                }
-                ?>
-            </tbody>
-        </table>
+            <div class="product-name">
+                <label for="pname">Product Name:</label>
+                <input type="text" id="pname" name="pname" placeholder="Enter Product Name" required>
+            </div>
+
+            <div class="product-price">
+                <label for="pprice">Product Price:</label>
+                <input type="text" id="pprice" name="pprice" placeholder="Enter Product Price" required>
+            </div>
+
+            <div class="product-image">
+                <label for="pimage">Add Product Image:</label>
+                <input type="file" id="pimage" name="pimage" required>
+            </div>
+
+            <div class="product-stock">
+                <label for="pstock">Product Stock:</label>
+                <input type="text" id="pstock" name="pstock" placeholder="Enter Stock Amount">
+            </div>
+
+            <div class="product-category">
+                <label for="pcat">Select Product Category:</label>
+                <select id="pcat" name="pcat" required>
+                    <option selected>Select Category</option>
+                    <?php
+                    include "config.php";
+                    $sql = "SELECT * FROM `category` WHERE status = 'active'";
+                    $result = $conn->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value='{$row['category_name']}'>{$row['category_name']}</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <button name="submit" type="submit" class="upload">Upload</button>
+        </form>
+
+        <div class="table-section">
+            <table>
+                <thead>
+                    <th>S.N.</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Stock</th>
+                    <th>Category</th>
+                    <th>Delete</th>
+                    <th>Update</th>
+                </thead>
+                <tbody>
+                    <?php
+                    include 'config.php';
+
+                    $record = mysqli_query($conn, "SELECT p.*, c.category_name 
+                                   FROM product p
+                                   INNER JOIN category c ON p.category_id = c.id");
+                    $count = 1;
+                    while ($row = mysqli_fetch_array($record)) {
+                        echo "
+            <tr>
+                <td>{$count}</td>
+                <td>$row[product_name]</td>
+                <td>$row[price]</td>
+                <td><img src='$row[product_image]' height='50px' width='50px'></td>
+                <td>$row[stock]</td>
+                <td>$row[category_name]</td>
+                <td>
+                    <div class='delete'>
+                        <a href='delete.php?id=" . $row['id'] . "'>Delete</a>
+                    </div>
+                </td>
+                <td>
+                    <div class='update'>
+                        <a href='update.php?id=" . $row['id'] . "'>Update</a>
+                    </div>
+                </td>
+            </tr>
+        ";
+                        $count += 1;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 
